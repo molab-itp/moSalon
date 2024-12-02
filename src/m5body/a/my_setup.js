f3ba26cb15658e3bb99ac526150c80e0fe4b1a19
John Henry Thompson <jht9629@nyu.edu>
2024-12-02 17:54:09 -0500

    81

--- src/m5body/a/my_setup.js
+++ src/m5body/a/my_setup.js
@@ -1,7 +1,7 @@
 //
 function my_init() {
   // updated to verify change on mobile
-  my.version = '?v=80';
+  my.version = '?v=81';
 
   my.reverseEffectOrder = 1;
   my.scroller_rate = 0.03;
--- src/mindverse-manju/a_sketch.js
+++ src/mindverse-manju/a_sketch.js
@@ -9,7 +9,7 @@ let my = {};
 
 function preload() {
   //
-  my.version = '?v=80';
+  my.version = '?v=81';
 
   my.backgImg = loadImage('../assets/Manju.jpg');
 
