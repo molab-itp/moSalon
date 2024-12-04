11b6b3ed2cee69e3f36f74074c23692d8089bf87
John Henry Thompson <jht9629@nyu.edu>
2024-12-04 04:53:28 -0500

    83

--- src/faceMesh/a_my_setup.js
+++ src/faceMesh/a_my_setup.js
@@ -1,7 +1,7 @@
 //
 function my_init() {
   // updated to verify change on mobile
-  my.version = '?v=82';
+  my.version = '?v=83';
   my.appTitle = 'Facemesh';
   my.isRemote = 1;
   // show detailed log
--- src/m5body/a/my_setup.js
+++ src/m5body/a/my_setup.js
@@ -1,7 +1,7 @@
 //
 function my_init() {
   // updated to verify change on mobile
-  my.version = '?v=82';
+  my.version = '?v=83';
 
   my.showEffectsEnabled = 0;
   my.reverseEffectOrder = 1;
