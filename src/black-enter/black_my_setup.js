87eee1114f9b84fb0685cb84487e3225bcc00b18
John Henry Thompson <jht9629@nyu.edu>
2025-01-28 15:59:03 -0500

    27

--- src/black-enter/black_my_setup.js
+++ src/black-enter/black_my_setup.js
@@ -4,7 +4,7 @@ function black_my_setup() {
   console.log('my_setup_black');
 
   my.appTitle = 'Blackfacts';
-  my.version = '?v=26';
+  my.version = '?v=27';
 
   my.fireb_config = 'jht9629';
   // my.fireb_config = 'jht1493';
--- src/mandala/manju/a_sketch.js
+++ src/mandala/manju/a_sketch.js
@@ -9,7 +9,7 @@ let my = {};
 
 function preload() {
   //
-  my.version = '?v=26';
+  my.version = '?v=27';
 
   my.backImage = loadImage('../../assets/Manju.jpg');
 
